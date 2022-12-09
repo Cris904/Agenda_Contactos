589eb1d87c6568c69239ffb860df9d92bb2f7075
Cris904 <cristiafl3@gmail.com>
2022-12-09 16:50:05 -0500

    version 2 genero

--- resources/js/Pages/Books/Create.vue
+++ resources/js/Pages/Books/Create.vue
@@ -112,6 +112,7 @@
                                         <select 
                                         v-model="form.gender"
                                         name="gender"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                         >
                                                     <option value="Masculino">Masculino</option>
                                                     <option value="Femenino">Femenino</option>
